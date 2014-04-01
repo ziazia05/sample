58019dce055c91b461f060f958f3ef84961506aa
ziazia05 <ziazia05@nate.com>
2014-04-01 13:11:45 +0900

    Update Sample.java

--- src/com/test/test/Sample.java
+++ src/com/test/test/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("출력출력");
+		System.out.println("출력출력++++++++++");
 	}
 
 }
